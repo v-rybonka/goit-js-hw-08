f7ae30fdb2693e6e7ca2e287fa638511b8320027
Valery <valeria.rybonka@gmail.com>
2022-08-20 20:40:35 +0300

    completed feedback form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,15 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
-const iframeEl = document.querySelector("iframe");
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframeEl = document.querySelector('iframe');
 const player = new Player(iframeEl);
 
-const STORAGE_KEY = "videoplayer-current-time";
+const STORAGE_KEY = 'videoplayer-current-time';
 
-console.log(+localStorage.getItem(STORAGE_KEY));
+console.log(localStorage.getItem(STORAGE_KEY) || 0);
 
-player.setCurrentTime(+localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
 
-player.on("timeupdate", throttle(onTimeupdate, 1000));
+player.on('timeupdate', throttle(onTimeupdate, 1000));
 
 function onTimeupdate(params) {
   localStorage.setItem(STORAGE_KEY, params.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const mail = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+const formData = {};
+
+populateTextarea();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback');
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  console.log(formData);
+  const formDataJson = JSON.stringify(formData);
+  console.log(formDataJson);
+  localStorage.setItem('feedback', formDataJson);
+}
+
+function populateTextarea(evt) {
+  const saveFormValue = JSON.parse(localStorage.getItem('feedback'));
+  if (saveFormValue) {
+    console.log(saveFormValue);
+    textarea.value = saveFormValue.message;
+    mail.value = saveFormValue.email;
+  }
+}
