d52248c7c9654d5abfa0684be218c4910472e894
Valery <valeria.rybonka@gmail.com>
2022-08-23 13:23:36 +0300

    Update 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,50 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const mail = document.querySelector('.feedback-form input');
-const textarea = document.querySelector('.feedback-form textarea');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  mail: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
-const formData = {};
+const STORAGE_KEY = 'feedback-form';
+let formValue = {};
 
 populateTextarea();
 
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  evt.currentTarget.reset();
-  localStorage.removeItem('feedback');
+  const formElements = evt.target.elements;
+  const mail = formElements.email.value;
+  const message = formElements.message.value;
+
+  const formData = {
+    mail,
+    message,
+  };
+
+  if (mail === '' || message === '') {
+    return alert('Please fill in the fields');
+  }
+  evt.target.reset();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  console.log(formData);
-  const formDataJson = JSON.stringify(formData);
-  console.log(formDataJson);
-  localStorage.setItem('feedback', formDataJson);
+  formValue[evt.target.name] = evt.target.value;
+
+  const formValueJson = JSON.stringify(formValue);
+  localStorage.setItem(STORAGE_KEY, formValueJson);
 }
 
-function populateTextarea(evt) {
-  const saveFormValue = JSON.parse(localStorage.getItem('feedback'));
-  if (saveFormValue) {
-    console.log(saveFormValue);
-    textarea.value = saveFormValue.message;
-    mail.value = saveFormValue.email;
+function populateTextarea() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    formValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    refs.mail.value = formValue.email ? formValue.email : '';
+    refs.textarea.value = formValue.message ? formValue.message : '';
   }
 }
